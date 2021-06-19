ca74c3555a8000a8dbab1e105f26ae6e6e8e607a
Thomas Stoeckert <Thomas@Stoeckert.us>
2021-06-18 20:51:12 -0400

    Refactoring / commenting

--- src/c/waittimes.c
+++ src/c/waittimes.c
@@ -18,6 +18,9 @@ const char* const park_groups[] = {
   "Disneyland Paris Resort"
 };
 
+// This could technically also be offloaded to the API, but I don't want the user
+// to wait twice each time they load the app (1x for loading the app, 1x for 
+// loading a park's times)
 const Park park_array[] =
     {
         // Park name is human-readable display text,
@@ -102,6 +105,52 @@ static void inbox_received_callback(DictionaryIterator *iter, void *context)
   }
 }
 
+static void send_park_request(int parkID)
+{
+  // We want to send a message to the javascript component.
+  // To do so, we do the following:
+  // 1. Create the payload (out_iter)
+  // 2. Initialize the outbox for our sending
+  DictionaryIterator *out_iter;
+  AppMessageResult result = app_message_outbox_begin(&out_iter);
+
+  if (result == APP_MSG_OK)
+  {
+    // 3. If the outbox is ready for us to send, we then need to prepare the payload
+    int parkID = park_array[parkID].id;
+    dict_write_int(out_iter, MESSAGE_KEY_o_parkID, &parkID, sizeof(int), true);
+
+    // 4. Send the message via the outbox.
+    result = app_message_outbox_send();
+
+    if (result != APP_MSG_OK)
+    {
+      // If there was an error in sending the message, oops.
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Error sending the outbox for %d: %d", parkID, (int)result);
+    }
+    else
+    {
+      // There was no error sending the message, so we can set our current state
+      // to "loading"
+      APP_LOG(APP_LOG_LEVEL_INFO, "Park loading request sent for %d", parkID);
+      s_are_attractions_loading = true;
+      // And update our selected park index (for the header on the details page)
+      s_selected_park_index = parkID;
+    }
+  }
+  else
+  {
+    // Outbox cannot be used right now
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Error preparing the outbox for %d: %d", parkID, (int)result);
+  }
+}
+
+/*
+ * --- Main Page / Park Select Interface ---
+ *
+ */
+/// The "onclick" functionality of the main menu layer. Essentially just
+/// issues the server request and opens the loading page.
 static void
 select_park_callback(struct MenuLayer *s_menu_layer, MenuIndex *cell_index,
                      void *callback_context)
@@ -109,46 +158,23 @@ select_park_callback(struct MenuLayer *s_menu_layer, MenuIndex *cell_index,
 
   APP_LOG(APP_LOG_LEVEL_INFO, "Park %s has been clicked", park_array[cell_index->row].name);
 
-  // Issue request to javascript
-  DictionaryIterator *out_iter;
-  s_are_attractions_loading = true;
-  AppMessageResult result = app_message_outbox_begin(&out_iter);
+  // Send the park load request
+  send_park_request(cell_index->row);
 
-  if (result == APP_MSG_OK)
-  {
-    // Construct the message
-    int parkID = park_array[cell_index->row].id;
-    s_selected_park_index = cell_index->row;
-    dict_write_int(out_iter, MESSAGE_KEY_o_parkID, &parkID, sizeof(int), true);
-
-    result = app_message_outbox_send();
-
-    if (result != APP_MSG_OK)
-    {
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Error sending the outbox: %d", (int)result);
-    }
-    else
-    {
-      APP_LOG(APP_LOG_LEVEL_INFO, "Message Sent");
-    }
-  }
-  else
-  {
-    // Outbox cannot be used right now
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Error preparing the outbox: %d", (int)result);
-  }
-
-  // Set the state of the park window to "loading"
-
-  // Push the park window
+  // Push the loading window
+  // TODO: Loading window
 }
 
+/// A handler function that just returns the number of parks (fixed, but taken
+/// from the parks array)
 static uint16_t get_parks_row_count(struct MenuLayer *menulayer, uint16_t section_index, void *callback_context)
 {
   int count = sizeof(park_array) / sizeof(Park);
   return count;
 }
 
+/// This function is responsible for drawing each row of the parks page - namely
+/// the various titles and subtitles.
 static void draw_parks_row_handler(GContext *ctx, const Layer *cell_layer,
                                    MenuIndex *cell_index, void *callback_context)
 {
@@ -161,6 +187,7 @@ static void draw_parks_row_handler(GContext *ctx, const Layer *cell_layer,
   menu_cell_basic_draw(ctx, cell_layer, name, subtitle, NULL);
 }
 
+/// This function establishes the main menu layer's properties and functionality
 static void parks_browse_load(Window *window)
 {
   // Get the root layer / bounds for this window
@@ -176,6 +203,7 @@ static void parks_browse_load(Window *window)
   // Bind the window's input functionality onto the menu
   menu_layer_set_click_config_onto_window(s_parks_menu_layer, window);
 
+  // Stylize the main menu layer
   menu_layer_set_highlight_colors(s_parks_menu_layer, GColorBulgarianRose, GColorWhite);
 
   // Add the menu layer as a child of the main window
@@ -187,11 +215,18 @@ static void parks_browse_unload(Window *window)
   menu_layer_destroy(s_parks_menu_layer);
 }
 
+/*
+ * --- Secondary Page / Attraction Browse Interface ---
+ *
+ */
+
+/// This is a simple handler function that returns the number of attractions in the attractions list.
 static uint16_t get_attractions_row_count(struct MenuLayer *menulayer, uint16_t section_index, void *callback_context)
 {
   return s_num_attractions;
 }
 
+/// This function renders a row for a given attraction (cell_index) in the attractions lists. 
 static void draw_attractions_row_handler(GContext *ctx, const Layer *cell_layer,
                                          MenuIndex *cell_index, void *callback_context)
 {
@@ -199,21 +234,27 @@ static void draw_attractions_row_handler(GContext *ctx, const Layer *cell_layer,
   menu_cell_basic_draw(ctx, cell_layer, s_attraction_names[i], s_attraction_status[i], NULL);
 }
 
+/// This function returns the default header height so the section headers render
 static int16_t get_attractions_header_height(struct MenuLayer *menulayer, uint16_t section_index, void *callback_context)
 {
   return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
+/// This function draws a simple header, with the title being the name of the park.
 static void draw_attractions_header(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *callback_context)
 {
   menu_cell_basic_header_draw(ctx, cell_layer, park_array[s_selected_park_index].name);
 }
 
+/// This function prepares the attraction list page menu, setting callbacks, and
+/// preparing the window for display.
 static void attraction_list_load(Window *window)
 {
+  // Get the root properties for our window (layer, bounds)
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create the attractions menu layer, assign callbacks for functionality
   s_attractions_menu_layer = menu_layer_create(bounds);
   menu_layer_set_callbacks(s_attractions_menu_layer, NULL, (MenuLayerCallbacks){
                                                                .get_num_rows = get_attractions_row_count,
@@ -221,17 +262,26 @@ static void attraction_list_load(Window *window)
                                                                .get_header_height = get_attractions_header_height,
                                                                .draw_header = draw_attractions_header
                                                            });
+  // Bind user input (up/down/select) to the menu layer
   menu_layer_set_click_config_onto_window(s_attractions_menu_layer, window);
 
+  // Stylize the menu layer
   menu_layer_set_highlight_colors(s_attractions_menu_layer, GColorBulgarianRose, GColorWhite);
 
+  // Establish the menu layer as a child of our root layer
   layer_add_child(window_layer, menu_layer_get_layer(s_attractions_menu_layer));
 }
 
+/// A little helper function that just destroys the created window
 static void attraction_list_unload(Window *window)
 {
   menu_layer_destroy(s_attractions_menu_layer);
 }
+
+/*
+ * --- Main app management functions ---
+ *
+ */
 
 static void init(void)
 {
@@ -251,6 +301,7 @@ static void init(void)
       s_attraction_list_window, (WindowHandlers){.load = attraction_list_load,
                                                  .unload = attraction_list_unload});
 
+  // Push our main menu window onto the stack.
   window_stack_push(s_parks_browse_window, false);
 }
 
