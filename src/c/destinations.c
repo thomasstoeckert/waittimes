f91df94774278ccbdadf2131250bf54a00c1014e
Thomas Stoeckert <Thomas@Stoeckert.us>
2025-08-09 14:24:18 -0400

    [+] Finshed save/load implementation of destinations data

--- src/c/destinations.c
+++ src/c/destinations.c
@@ -6,6 +6,7 @@ static char s_park_ids[I_MAX_PARKS][I_PARK_UUID_LENGTH];
 static char s_destination_names[I_MAX_DESTINATIONS][I_MAX_DESTINATION_NAME_LENGTH];
 static int i_park_destination[I_MAX_PARKS];
 static int i_park_count;
+static int i_destination_count;
 
 int parse_destinations_response(DictionaryIterator *iter, void *context){
     // We've been called. We need to look for a message with our new park count
@@ -52,7 +53,7 @@ int parse_destinations_response(DictionaryIterator *iter, void *context){
 
         // We're good! Go through and extract our data for this park
         strncpy(s_park_names[newpark_index], parkname_tuple->value->cstring, I_MAX_PARK_NAME_LENGTH - 1);
-        strncpy(s_park_ids[newpark_index], parkid_tuple->value->cstring, I_PARK_UUID_LENGTH );
+        strncpy(s_park_ids[newpark_index], parkid_tuple->value->cstring, I_PARK_UUID_LENGTH - 1);
         i_park_destination[newpark_index] = destid_tuple->value->int32;
 
         // Next, see if we don't have this destination index yet.
@@ -80,6 +81,8 @@ int parse_destinations_response(DictionaryIterator *iter, void *context){
         strncpy(s_destination_names[destination_index], destname_tuple->value->cstring, I_MAX_DESTINATION_NAME_LENGTH - 1);
         APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: D{%2d}%s", destination_index, s_destination_names[destination_index]);
     }
+    // Store our maximum destination count
+    i_destination_count = i_max_destid;
 
     // We've got it all!
     APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: Finished parsing all destination information");
@@ -120,4 +123,177 @@ int get_data_for_park_index(int park_index, ParkData *park_data)
     park_data->park_uuid = s_park_ids[park_index];
     park_data->destination_name = s_destination_names[i_park_destination[park_index]];
     return 0;
+}
+
+void persist_save_destinations_data()
+{
+    // Prepare metadata block
+    PersistDestMetadata persist_dest_metadata = (PersistDestMetadata) 
+    {
+        .num_parks = i_park_count,
+        .num_destinations = i_destination_count,
+        .version = PERSIST_VERSION_DESTINATIONS
+    };
+
+    // Store metadata block
+    persist_write_data(PERSIST_KEY_DESTINATIONS_METADATA, &persist_dest_metadata, sizeof(PersistDestMetadata));
+
+    // Store park data
+    PersistParkData working_park_persist[4];
+    int source_park_index = 0;
+    while (source_park_index < i_park_count)
+    {
+        int working_index = source_park_index % 4;
+        
+        // For each park, put it into the working_park_persist array
+        working_park_persist[working_index].park_destid_persist = i_park_destination[source_park_index];
+
+        strncpy(working_park_persist[working_index].park_name_persist,
+            s_park_names[source_park_index], I_MAX_PARK_NAME_LENGTH - 1);
+
+        strncpy(working_park_persist[working_index].park_uuid_persist,
+            s_park_ids[source_park_index], I_PARK_UUID_LENGTH - 1);
+        
+        if(source_park_index % 4 == 3)
+        {
+            // Save
+            int persist_key_offset = source_park_index / 4;
+            persist_write_data(PERSIST_KEY_PARKS_BASE + persist_key_offset, 
+                working_park_persist, sizeof(PersistParkData) * 4);
+        }
+
+        source_park_index += 1;
+    }
+
+    // If the park count didn't divide evenly into sets of four parks, the last
+    // set of (1-3) won't have been stored. Store that.
+    if(i_park_count % 4 != 0) 
+    {
+        int persist_key_offset = i_park_count / 4;
+        persist_write_data(PERSIST_KEY_PARKS_BASE + persist_key_offset,
+            working_park_persist, sizeof(PersistParkData) * 4);
+    }
+
+    // Store destinations data
+    //
+    // We can store six destination names in a 256-byte block 
+    // (37 bytes per name) = 222 bytes
+    char working_destnames[6][I_PARK_UUID_LENGTH];
+    // Store'm
+    for (int i = 0; i < i_destination_count; i++) 
+    {
+        int working_destnames_index = i % 6;
+        
+        strncpy(working_destnames[working_destnames_index], 
+            s_destination_names[i], I_MAX_DESTINATION_NAME_LENGTH - 1);
+            
+        if(working_destnames_index == 5)
+        {
+            int persist_key_offset = i / 6;
+            persist_write_data(PERSIST_KEY_DESTINATIONS_BASE + persist_key_offset, 
+                working_destnames, sizeof(char) * I_PARK_UUID_LENGTH * 6);
+        }
+    }
+
+    APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: Stored %d Parks from %d Destinations into storage", i_park_count, i_destination_count);
+}
+
+void persist_load_destinations_data()
+{
+    // Get metadata block
+    PersistDestMetadata persist_dest_metadata;
+    if(!persist_exists(PERSIST_KEY_DESTINATIONS_METADATA))
+    {
+        // If it doesn't exist, we can't continue. Print an error, and fail
+        APP_LOG(APP_LOG_LEVEL_ERROR, "[D.C]: Unable to find any destinations data in storage. Not continuing load.");
+        return;
+    }
+    // APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: Found metadatablock. Loading persist data from storage");
+    
+    persist_read_data(PERSIST_KEY_DESTINATIONS_METADATA, &persist_dest_metadata, sizeof(PersistDestMetadata));
+    
+    // Print out that we've got it
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "[D.C]: Got the metadata block. Looking for %d parks and %d destination names, v%d",
+    //     persist_dest_metadata.num_parks, persist_dest_metadata.num_destinations,
+    //     persist_dest_metadata.version);
+
+    if(persist_dest_metadata.version != PERSIST_VERSION_DESTINATIONS)
+    {
+        APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: Version loaded from metadata block doesn't match expected version. %d != %d.", persist_dest_metadata.version, PERSIST_VERSION_DESTINATIONS);
+        APP_LOG(APP_LOG_LEVEL_WARNING, "[D.C]: No 'upgrade' functionality implemented. Returning.");
+        return;
+    }
+    
+    i_park_count = persist_dest_metadata.num_parks;
+    i_destination_count = persist_dest_metadata.num_destinations;
+
+    // Load parks data
+    // Parks data is stored in 4-park data blocks. Estimate the number of data
+    // blocks we should read
+    int expected_park_blocks = i_park_count / 4 + (i_park_count % 4 != 0);
+    // APP_LOG(APP_LOG_LEVEL_DEBUG, "I think we'll be seeing %d blocks of 4 parks, since we have %d total parks", expected_park_blocks, i_park_count);
+    int park_idx = 0;
+    for (int i = 0; i < expected_park_blocks; i++)
+    {
+        // Load a park block
+        PersistParkData working_park_persist[4];
+        persist_read_data(PERSIST_KEY_PARKS_BASE + i, working_park_persist, 
+            sizeof(PersistParkData) * 4);
+
+        
+        // Pull out the amount of data I expect to be in it
+        int remaining_count = i_park_count - park_idx;
+        // Clamp between 0 and 4
+        int set_count = remaining_count;
+        if(set_count > 4)
+        {
+            set_count = 4;
+        }
+
+        // APP_LOG(APP_LOG_LEVEL_DEBUG, "Read a block from %d! I think we'll pull %d parks from it", PERSIST_KEY_PARKS_BASE + i, set_count);
+        for (int j = 0; j < set_count; j++)
+        {
+            // Store that in big original array
+            i_park_destination[park_idx] = working_park_persist[j].park_destid_persist;
+            strncpy(s_park_names[park_idx], working_park_persist[j].park_name_persist, I_MAX_PARK_NAME_LENGTH - 1);
+            strncpy(s_park_ids[park_idx], working_park_persist[j].park_uuid_persist, I_PARK_UUID_LENGTH - 1);
+            // APP_LOG(APP_LOG_LEVEL_DEBUG, "Read a park! Here's its data: ");
+            // APP_LOG(APP_LOG_LEVEL_DEBUG, "  Index: %d", park_idx);
+            // APP_LOG(APP_LOG_LEVEL_DEBUG, "  DestIDX: %d", i_park_destination[park_idx]);
+            // APP_LOG(APP_LOG_LEVEL_DEBUG, "  Name: %s", s_park_names[park_idx]);
+            // APP_LOG(APP_LOG_LEVEL_DEBUG, "  Id: %s", s_park_ids[park_idx]);
+            park_idx += 1;
+        }
+    }
+
+    // Load destinations data
+    // Dest data is stored in 6-name blocks. Estimate the number of
+    // data blocks that should be read
+    int expected_dest_blocks = i_destination_count / 6 + (i_destination_count % 6 != 0);
+    int dest_idx = 0;
+    for (int i = 0; i < expected_dest_blocks; i++)
+    {
+        // Load a dest block
+        char working_destnames[6][I_PARK_UUID_LENGTH];
+        persist_read_data(PERSIST_KEY_DESTINATIONS_BASE + i,
+            working_destnames, sizeof(char) * I_PARK_UUID_LENGTH * 6);
+
+        // Pull our the number of names we expect to be in it
+        int remaining_count = i_destination_count - dest_idx;
+        int set_count = remaining_count;
+        if(set_count > 6)
+        {
+            set_count = 6;
+        }
+
+        // Store that in the big array
+        for (int j = 0; j < set_count; j++)
+        {
+            strncpy(s_destination_names[dest_idx], working_destnames[j], 
+                I_MAX_DESTINATION_NAME_LENGTH - 1);
+            dest_idx += 1;
+        }
+    }
+
+    APP_LOG(APP_LOG_LEVEL_INFO, "[D.C]: Loaded %d Parks from %d Destinations from storage", i_park_count, i_destination_count);
 }
--- src/c/destinations.h
+++ src/c/destinations.h
@@ -3,12 +3,17 @@
 #include <pebble.h>
 
 #define I_MAX_PARKS 32
-#define I_MAX_PARK_NAME_LENGTH 33
+#define I_MAX_PARK_NAME_LENGTH 23
 #define I_PARK_UUID_LENGTH 37
 #define I_MAX_DESTINATION_NAME_LENGTH I_MAX_PARK_NAME_LENGTH
 #define I_MAX_DESTINATIONS I_MAX_PARKS
 
 #define DESTINATIONS_ERROR -1
+
+#define PERSIST_VERSION_DESTINATIONS 1
+#define PERSIST_KEY_DESTINATIONS_METADATA 4
+#define PERSIST_KEY_PARKS_BASE 5
+#define PERSIST_KEY_DESTINATIONS_BASE PERSIST_KEY_PARKS_BASE + 8
 
 typedef struct {
     char * park_name;
@@ -18,6 +23,18 @@ typedef struct {
 
 #define ParkData(name, uuid, destname) ((ParkData){(name), (uuid), (destname)});
 
+typedef struct {
+    int num_parks;
+    int num_destinations;
+    int version;
+} __attribute__((__packed__)) PersistDestMetadata;
+
+typedef struct {
+    char park_uuid_persist[I_PARK_UUID_LENGTH];
+    char park_name_persist[I_MAX_PARK_NAME_LENGTH];
+    int park_destid_persist;
+} __attribute__((__packed__)) PersistParkData;
+
 int parse_destinations_response(DictionaryIterator *iter, void *context);
 
 void print_destinations();
@@ -26,3 +43,7 @@ int get_park_count();
 
 int get_data_for_park_index(int park_index, ParkData* park_data);
 
+void persist_save_destinations_data();
+void persist_load_destinations_data();
+// void persist_upgrade_destinations_data();
+// bool persist_has_destinations_data();
